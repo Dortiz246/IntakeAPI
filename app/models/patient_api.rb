a641746800c8fa317eb5ac57b17efbf202cd251d
Dortiz246 <ortizdarryl@gmail.com>
2023-11-14 11:43:22 -0500

    referred to the table of the other form app

--- app/models/patient_api.rb
+++ app/models/patient_api.rb
@@ -1,4 +1,5 @@
 class PatientApi < ApplicationRecord
+    self.table_name = "patients"
     validates :first_name, presence: true
     validates :last_name, presence: true
     validates :dob, presence: true
