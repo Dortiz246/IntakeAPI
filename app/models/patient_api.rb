f7e94a76bbf2f181ad9f10fe9c247d85964c62bb
Dortiz246 <ortizdarryl@gmail.com>
2023-11-14 12:05:27 -0500

    removed fields that conflicted with original table

--- app/models/patient_api.rb
+++ app/models/patient_api.rb
@@ -4,8 +4,8 @@ class PatientApi < ApplicationRecord
     validates :last_name, presence: true
     validates :dob, presence: true
     validates :phone_number, presence: true
-    validates :address, presence: true
-    validates :city, presence: true
-    validates :state, presence: true
-    validates :zip_code, presence: true
+    #validates :address, presence: true
+    #validates :city, presence: true
+    #validates :state, presence: true
+    #validates :zip_code, presence: true
 end
